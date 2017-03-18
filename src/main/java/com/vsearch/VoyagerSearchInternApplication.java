574c12f758b52aa0556bf9a2722d6c9c40c3a38c
stephenpaul2727 <stephenpaul2727@gmail.com>
2017-03-18 01:32:29 -0400

    Initial Commit
    Spring data solr added.
    Spring boot project structure added.

---
+++ src/main/java/com/vsearch/VoyagerSearchInternApplication.java
@@ -0,0 +1,12 @@
+package com.vsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoyagerSearchInternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoyagerSearchInternApplication.class, args);
+	}
+}
